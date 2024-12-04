b14b46a8eaaf4934a8b1c1427d0be915b7530370
JuanPabaz <jpgthf@gmail.com>
2024-12-04 17:40:07 -0500

    feat: eliminar funciones inncesarias para manejar los mensajes de error

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthenticationControllerService } from '../../services/services';
 import { HttpClient } from '@angular/common/http';
+import { TokenService } from '../../services/token/token.service';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,8 @@ export class LoginComponent {
   errorMsg: Array<string> = [];
 
   constructor(private router:Router, 
-    private authService: AuthenticationControllerService) { 
+    private authService: AuthenticationControllerService,
+    private tokenService: TokenService) { 
   
   }
 
@@ -26,41 +28,16 @@ export class LoginComponent {
     this.authService.getToken({ body: this.authRequest })
       .subscribe({
         next: (response) => {
+          debugger
+          this.tokenService.token = response.accessToken as string;
           this.router.navigate(['/vehiculos']);
         },
         error: (errorResponse) => {
-          this.handleErrorResponse(errorResponse);
+          debugger
+          console.error(errorResponse.error);
+          this.errorMsg.push(errorResponse.error.message);
         }
       });
-  }
-
-  handleErrorResponse(errorResponse: any) {
-    if (errorResponse.error instanceof Blob) {
-      // Si el error es un Blob, lo convertimos
-      this.convertBlobToMessage(errorResponse.error);
-    } else if (errorResponse.error && errorResponse.error.message) {
-      // Si el error tiene un mensaje, lo mostramos directamente
-      this.errorMsg.push(errorResponse.error.message);
-    } else {
-      // En otros casos, mostramos un mensaje genérico
-      this.errorMsg.push(`Error ${errorResponse.status}: ${errorResponse.statusText || 'Desconocido'}`);
-    }
-  }
-
-  // Función para convertir un Blob en un mensaje y agregarlo a errorMsg
-  convertBlobToMessage(blob: Blob) {
-    const reader = new FileReader();
-    reader.onload = () => {
-      try {
-        const errorMessage = JSON.parse(reader.result as string); // Convierte el blob a JSON
-        this.errorMsg.push(errorMessage.message); // Muestra el mensaje de error
-      } catch (e) {
-        this.errorMsg.push("Error al procesar el mensaje de error.");
-      }
-    };
-
-    // Lee el Blob como texto
-    reader.readAsText(blob);
   }
 
   register(){
