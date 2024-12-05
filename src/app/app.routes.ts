742fefd80d8f91731aa4469ec4eaec9c75ec5da8
JuanPabaz <jpgthf@gmail.com>
2024-12-05 08:31:46 -0500

    feat: añadir la ruta para el component de vehiculos y registrarse

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { VehiculosComponent } from './pages/vehiculos/vehiculos.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'vehiculos',
+        component: VehiculosComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
     }
 ];
