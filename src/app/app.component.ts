6d1f24133babcfa905cc6ab9b325e0c1bf5d6579
JuanPabaz <jpgthf@gmail.com>
2024-12-05 15:54:03 -0500

    Añadir menu componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
